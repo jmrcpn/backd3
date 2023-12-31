bd08e2b272a3dc2b8eced8599eface62fb0747a6
Jean-Marc Pigeon (Delson) <jmp@safe.ca>
2023-12-31 14:39:58 -0500

    Using backd3

--- lib/subrou.c
+++ lib/subrou.c
@@ -19,7 +19,7 @@
 #include	"subrou.h"
 
 #define VERSION "3.1"
-#define RELEASE "0.7"
+#define RELEASE "0.8"
 
 #define APPLICATION             "backd"
 #define DIRLOCK                 "/var/run/"APPLICATION
